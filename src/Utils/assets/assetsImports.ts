21bf5067d6bb13d9f93773af10dfc8648395e8bc
ofekbd-moveo <ofekbd@moveo.co.il>
2021-10-24 09:58:06 +0300

    fix DispatcherPage and HomePage pr

--- src/Utils/assets/assetsImports.ts
+++ src/Utils/assets/assetsImports.ts
@@ -6,12 +6,12 @@ import noFound from "../assets/not-found.svg";
 import chart from "../assets/chart.svg";
 
 const assets = {
-  arrowRight: arrowRight,
-  logo: logo,
-  search: search,
-  dropDownArrow: dropDownArrow,
-  noFound: noFound,
-  chart: chart,
+  arrowRight,
+  logo,
+  search,
+  dropDownArrow,
+  noFound,
+  chart,
 };
 
 export default assets;
